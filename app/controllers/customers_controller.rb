ba9ea284c5b8b8fbed8d865b18b45e3ed7325f87
Lee Yi Hui <triceo@gmail.com>
2016-10-10 22:07:30 +0800

    add customer to queue

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,8 +10,16 @@ class CustomersController < ApplicationController
 
   def create
     @user = User.find_or_create_by!(user_params)
-    
-    redirect_to '/'
+    @customer = Customer.new(store_id: params[:store_id])
+    @customer.user_id = @user.id
+    queue = Customer.where(store_id: params[:store_id]).maximum('queue_no') + 1
+    @customer.queue_no = queue
+    @customer.status_id = 1
+    if @customer.save!
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
 
   end
 
