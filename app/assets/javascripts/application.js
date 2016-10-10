3f8f65addd76feb491ae73a7d687bc149bf72cd5
Phua Kuan Yu <Phuaky@virus.local>
2016-10-10 11:54:42 +0800

    dialog for joining queue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-var dialog = document.querySelector('dialog');
-    var showDialogButton = document.querySelector('#show-dialog');
-    if (! dialog.showModal) {
-      dialogPolyfill.registerDialog(dialog);
-    }
-    showDialogButton.addEventListener('click', function() {
-      dialog.showModal();
-    });
-    dialog.querySelector('.close').addEventListener('click', function() {
-      dialog.close();
-    });
