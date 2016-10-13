e2a3b77af2ddc1b96fd5d8b2a52257bd8e5f526c
Lee Yi Hui <triceo@gmail.com>
2016-10-13 11:48:24 +0800

    add new store form revamped

--- app/assets/javascripts/form_validation.js
+++ app/assets/javascripts/form_validation.js
@@ -77,6 +77,10 @@ $(document).ready(function (){
                                 rules:  [{  type: 'integer',
                                             prompt: 'Please enter only numbers'}]
                                     },
+      max_queue_allow: { identifier: 'store[max_queue_allow]',
+                                rules:  [{  type: 'integer',
+                                            prompt: 'Please enter only numbers'}]
+                                    },
       max_leeway:{ identifier: 'store[max_leeway]',
                                 rules:  [{  type: 'integer',
                                             prompt: 'Please enter only numbers'}]
@@ -88,6 +92,68 @@ $(document).ready(function (){
       }
   });
 
+
+
+  $('#new-store-details').form({
+    inline: true,
+    on: 'blur',
+    fields: {
+      email:  { identifier: 'registered_user[email]',
+                                rules:  [{  type: 'empty',
+                                            prompt: 'Field must not be empty'},
+                                          { type: 'email',
+                                            prompt: 'Please enter a valid email'}]
+              },
+      password: { identifier: 'registered_user[password]',
+                                rules:  [{  type: 'empty',
+                                            prompt: 'Field must not be empty'},
+                                          { type: 'minLength[8]',
+                                            prompt: 'Your password must be at least {ruleValue} characters'},
+                                          { type: 'maxLength[72]',
+                                            prompt: 'Your superhuman memory is noted, but passwords cannot exceed {ruleValue} characters'}]
+                },
+      password_confirmation:  { identifier: 'registered_user[password_confirmation]',
+                                rules:  [{  type: 'empty',
+                                            prompt: 'Field must not be empty'},
+                                          { type: 'minLength[8]',
+                                            prompt: 'Your password must be at least {ruleValue} characters'}]
+                              },
+      store_name:  { identifier: 'store[store_name]',
+                                rules:  [{  type: 'empty',
+                                            prompt: 'Field must not be empty'}]
+              },
+      store_address: { identifier: 'store[store_address]',
+                                rules:  [{  type: 'empty',
+                                            prompt: 'Field must not be empty'}]
+                },
+      contact_person:  { identifier: 'store[contact_person]',
+                                rules:  [{  type: 'empty',
+                                            prompt: 'Field must not be empty'}]
+                              },
+      contact_no: { identifier: 'store[contact_no]',
+                                rules:  [{  type: 'integer',
+                                            prompt: 'Please enter only numbers'},
+                                          { type: 'regExp[/^(8|9)\\d{7}$/]',
+                                            prompt: 'Please enter a valid Singapore mobile number without country code'}]
+                    },
+      max_queue_no: { identifier: 'store[max_queue_no]',
+                                rules:  [{  type: 'integer',
+                                            prompt: 'Please enter only numbers'}]
+                                    },
+      max_queue_allow: { identifier: 'store[max_queue_allow]',
+                                rules:  [{  type: 'integer',
+                                            prompt: 'Please enter only numbers'}]
+                                    },
+      max_leeway:{ identifier: 'store[max_leeway]',
+                                rules:  [{  type: 'integer',
+                                            prompt: 'Please enter only numbers'}]
+                      },
+      reservation_fee: { identifier: 'store[reservation_fee]',
+                                rules:  [{  type: 'decimal',
+                                            prompt: 'Please enter a valid decimal number'}]
+                      }
+      }
+  });
 
 
 
